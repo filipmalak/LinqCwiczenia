051a36f67b5cbb8da534a66e63cbb2413e8d06ff
filipmalak <s28494@pjwstk.edu.pl>
2024-05-16 19:49:54 +0200

    Initial commit

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            foreach (var employee in t)
+            {
+                Console.WriteLine($"Empno: {employee.Empno}, Ename: {employee.Ename}, Job: {employee.Job}, Salary: {employee.Salary}");
+            }
+            
+            var a = LinqTasks.Task2();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
